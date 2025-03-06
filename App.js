bd87645547fc059de47950f173c00c146dcebd04
auhmaugmaufm <supitcha.aq@gmail.com>
2025-03-06 22:38:34 +0700

    restart

--- App.js
+++ App.js
@@ -1,28 +1,14 @@
-import React from "react";
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./src/screens/HomeScreen";
-import SignUp from "./src/screens/SignUp";
 import Login from "./src/screens/Login";
-<<<<<<< Updated upstream
 import BookingHistoryScreen from "./src/screens/BookingHistoryScreen";
 import BookingScreen from "./src/screens/BookingScreen";
 import EditScreen from "./src/screens/EditScreen";
-=======
-import SelectScreen from "./src/screens/SelectScreen";
->>>>>>> Stashed changes
 
 const Stack = createStackNavigator()
 
 const App = () => {
     return (
         <NavigationContainer>
-<<<<<<< Updated upstream
-            <Stack.Navigator initialRouteName="Booking">
-=======
-            <Stack.Navigator initialRouteName="Select">
->>>>>>> Stashed changes
+            <Stack.Navigator initialRouteName="Edit">
                 <Stack.Screen
                     name="Home"
                     component={HomeScreen}
@@ -39,7 +25,6 @@ const App = () => {
                     options={{ headerMode: 'none' }}
                 />
                 <Stack.Screen
-<<<<<<< Updated upstream
                     name="Booking"
                     component={BookingScreen}
                     options={{ headerMode: 'none' }}
@@ -49,15 +34,11 @@ const App = () => {
                     component={BookingHistoryScreen}
                     options={{ headerMode: 'none' }}
                 />
+
                 <Stack.Screen
                     name="Edit"
                     component={EditScreen}
                     options={{ headerMode: 'none' }}
-=======
-                    name="Select"
-                    component={SelectScreen}
-                    // options={{ headerMode: 'none' }}
->>>>>>> Stashed changes
                 />
             </Stack.Navigator>
         </NavigationContainer>
