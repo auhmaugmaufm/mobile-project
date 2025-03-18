34fd2d4daee1d3b0eca16a0c776f681c0926ae9e
I don't know <kittabeth2547@gmail.com>
2025-03-18 21:47:35 +0700

    filter

---
+++ frontend/src/components/Dropdown.js
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+
+const DropdownComponent = ({ name, rawData = [], onSelect }) => {
+    const [value, setValue] = useState(null);
+    const [isFocus, setIsFocus] = useState(false);
+    console.log('raw : ', rawData);
+
+    const data = rawData.map((item) => ({
+        label: item,
+        value: item
+    }));
+
+    const clearData = () => {
+        setValue(null)
+        onSelect('')
+    }
+
+    const renderLabel = () => {
+        if (value || isFocus) {
+            return (
+                <Text style={[styles.label, isFocus && { color: 'blue' }]}>
+                    {name}
+                </Text>
+            );
+        }
+        return null;
+    };
+
+    return (
+        <View style={styles.container}>
+            {renderLabel()}
+            <Dropdown
+                style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
+                placeholderStyle={styles.placeholderStyle}
+                selectedTextStyle={styles.selectedTextStyle}
+                inputSearchStyle={styles.inputSearchStyle}
+                iconStyle={styles.iconStyle}
+                data={data}
+                search
+                maxHeight={300}
+                labelField='label'
+                valueField='value'
+                placeholder={!isFocus ? name : '...'}
+                searchPlaceholder="Search..."
+                value={value}
+                onFocus={() => setIsFocus(true)}
+                onBlur={() => setIsFocus(false)}
+                onChange={item => {
+                    setValue(item);
+                    setIsFocus(false);
+                    if (onSelect) {
+                        onSelect(item.value)
+                    }
+                }}
+                renderLeftIcon={() => (
+                    <AntDesign
+                        style={styles.icon}
+                        color={isFocus ? 'blue' : 'black'}
+                        name="Safety"
+                        size={20}
+                    />
+                )}
+            />
+            {
+                value ?
+                    <TouchableOpacity onPress={clearData} style={{ position: 'absolute', right: 0 }}>
+                        <Icon name="cancel" size={30} color="#ccc" />
+                    </TouchableOpacity> :
+                    null
+            }
+        </View>
+    );
+};
+
+export default DropdownComponent;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        padding: 16,
+    },
+    dropdown: {
+        height: 50,
+        borderColor: 'gray',
+        borderWidth: 0.5,
+        borderRadius: 8,
+        paddingHorizontal: 8,
+    },
+    icon: {
+        marginRight: 5,
+    },
+    label: {
+        position: 'absolute',
+        backgroundColor: 'white',
+        left: 22,
+        top: 8,
+        zIndex: 999,
+        paddingHorizontal: 8,
+        fontSize: 14,
+    },
+    placeholderStyle: {
+        fontSize: 16,
+    },
+    selectedTextStyle: {
+        fontSize: 16,
+    },
+    iconStyle: {
+        width: 20,
+        height: 20,
+    },
+    inputSearchStyle: {
+        height: 40,
+        fontSize: 16,
+    },
+});
